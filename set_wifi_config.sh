1eeef698d50d3e6d37cf53ddf2c62a0aae29f805
Will Jack <will@remedymedical.com>
2020-03-03 01:31:26 -0800

    trying to get changes to stick

--- set_wifi_config.sh
+++ set_wifi_config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo cp /etc/network/interfaces /etc/network/interfaces_backup
 sudo cat ./networking_configs/interfaces > /etc/network/interfaces
+sudo chmod +x /etc/network/interfaces
 
 sudo cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant_backup.conf
 sudo cat ./networking_configs/wpa_supplicant.conf > /etc/wpa_supplicant/wpa_supplicant.conf
+sudo chmod +x /etc/wpa_supplicant/wpa_supplicant.conf
