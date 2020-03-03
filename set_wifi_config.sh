7b792fbd09cb22daa8c7fbeef0ab8e59c585235b
Will Jack <will@remedymedical.com>
2020-03-03 01:15:38 -0800

    rewrite files rather than copy them

--- set_wifi_config.sh
+++ set_wifi_config.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-sudo mv /etc/network/interfaces /etc/network/interfaces_backup
-sudo cp ./networking_configs/interfaces /etc/network/interfaces
-sudo chmod 644 /etc/network/interfaces
+sudo cp /etc/network/interfaces /etc/network/interfaces_backup
+sudo cat ./networking_configs/interfaces > /etc/network/interfaces
 
-sudo mv /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant_backup.conf
-sudo cp ./networking_configs/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
-sudo chmod 777 /etc/wpa_supplicant/wpa_supplicant.conf
+sudo cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant_backup.conf
+sudo cat ./networking_configs/wpa_supplicant.conf > /etc/wpa_supplicant/wpa_supplicant.conf
