348c6e87aba94fbb8fda9fa264f8ff57206e8d26
Will Jack <will@remedymedical.com>
2020-03-03 12:04:15 -0800

    new strategy... dont' tough interfaces, just change wpa_supplicant

--- set_wifi_config.sh
+++ set_wifi_config.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-sudo cp /etc/network/interfaces /etc/network/interfaces_backup
-sudo cat ./networking_configs/interfaces > /etc/network/interfaces
-sudo chmod +x /etc/network/interfaces
-
 sudo cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant_backup.conf
 sudo cat ./networking_configs/wpa_supplicant.conf > /etc/wpa_supplicant/wpa_supplicant.conf
 sudo chmod +x /etc/wpa_supplicant/wpa_supplicant.conf
+sudo sync
