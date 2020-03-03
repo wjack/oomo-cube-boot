45757dde824cfd063abb654da2815aff2cee504c
Will Jack <will@remedymedical.com>
2020-03-03 01:04:04 -0800

    changes file permissions

--- set_wifi_config.sh
+++ set_wifi_config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo mv /etc/network/interfaces /etc/network/interfaces_backup
 sudo cp ./networking_configs/interfaces /etc/network/interfaces
+sudo chmod 644 /etc/network/interfaces
 
 sudo mv /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant_backup.conf
 sudo cp ./networking_configs/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
-
+sudo chmod 777 /etc/wpa_supplicant/wpa_supplicant.conf
